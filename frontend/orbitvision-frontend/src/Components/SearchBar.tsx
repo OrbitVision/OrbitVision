40328b1d9b0d895bf8aeafddb21354240db16e57
Bejter <kacper.majda62@gmail.com>
2026-06-20 21:23:39 +0200

    Added Button Search

--- frontend/orbitvision-frontend/src/Components/SearchBar.tsx
+++ frontend/orbitvision-frontend/src/Components/SearchBar.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 
 export default function SearchBar(){
+
+    const [searchInput, setSearchInput] = useState("");
+
+    const handle = () => {
+        
+    }
+
     return ( 
         <>
         <div className="bg-black">
             <form>
-                <input type="text" className="bg-white " name="searchBarInput" placeholder="Wyszukaj satelitę"  ></input>
+                {/* Wyszukiwanie input */}
+                <input 
+                type="text" 
+                className="bg-white " 
+                name="searchBarInput" 
+                value={searchInput} 
+                onChange={(e) => setSearchInput(e.target.value)} 
+                placeholder="Wyszukaj satelitę"></input>
+
+                {/* Wyszukiwanie button */}
+                <input 
+                type="button" 
+                className="w-auto
+                 bg-white " 
+                 value={"Wyszukaj"} 
+                 name="searchBarButton" 
+                 onClick={handle}></input>
             </form>
         </div>
         </>
