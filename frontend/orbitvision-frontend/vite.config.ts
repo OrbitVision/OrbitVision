c2c8abb2bb090b717e538bf5ff98ca9b71ea8e39
Jakub Król <kozak10555@gmail.com>
2026-07-05 22:14:15 +0200

    fix

--- frontend/orbitvision-frontend/vite.config.ts
+++ frontend/orbitvision-frontend/vite.config.ts
@@ -2,17 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import cesium from 'vite-plugin-cesium';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     cesium()
-  ],
-  resolve: {
-    alias: {
-      'node:module': path.resolve(__dirname, './src/empty-module.js')
-    }
-  }
+  ]
 })
